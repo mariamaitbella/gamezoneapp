47b8f155c8c6398db0fbf2bd6f9ec093621378d1
Mariam Ait Bella <mariam.aitbella200@gmail.com>
2022-05-13 18:23:25 +0100

    adding files and packages

--- screens-last/about.js
+++ screens-last/about.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { globalStyles } from './styles/global';
 
 export default function About() {
   return (
-    <View style={globalStyles.container}>
+    <View style={styles.container}>
       <Text>About Screen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
--- screens/home.js
+++ screens/home.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+  const pressHandler = () => {
+    //navigation.navigate('ReviewDetails');
+    navigation.push('ReviewDetails');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title='to review details screen' onPress={pressHandler} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-});
